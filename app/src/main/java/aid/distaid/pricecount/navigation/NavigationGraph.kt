23b2653fd0292f6d039e669d481efecef1be9090
Distaid <88453698+Distaid@users.noreply.github.com>
2023-07-30 20:50:37 +0300

    Removed navigation animation

--- app/src/main/java/aid/distaid/pricecount/navigation/NavigationGraph.kt
+++ app/src/main/java/aid/distaid/pricecount/navigation/NavigationGraph.kt
@@ -3,34 +3,54 @@ package aid.distaid.pricecount.navigation
 import aid.distaid.pricecount.ui.screens.CreateItemScreen
 import aid.distaid.pricecount.ui.screens.EditProductScreen
 import aid.distaid.pricecount.ui.screens.HomeScreen
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun NavigationGraph(
     navHostController: NavHostController
 ) {
-    NavHost(
+    AnimatedNavHost(
         navController = navHostController,
         startDestination = NavigationItem.Home.route,
     ) {
-        composable(NavigationItem.Home.route) {
+        composable(
+            NavigationItem.Home.route,
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
+            popEnterTransition = { EnterTransition.None },
+            popExitTransition = { ExitTransition.None }
+        ) {
             HomeScreen(
                 navHostController
             )
         }
-        composable(NavigationItem.CreateProduct.route) {
+        composable(
+            NavigationItem.CreateProduct.route,
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
+            popEnterTransition = { EnterTransition.None },
+            popExitTransition = { ExitTransition.None }
+        ) {
             CreateItemScreen(
                 navHostController
             )
         }
         composable(
             NavigationItem.EditProduct.route,
-            arguments = listOf(navArgument("id") { type = NavType.IntType })
+            arguments = listOf(navArgument("id") { type = NavType.IntType }),
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
+            popEnterTransition = { EnterTransition.None },
+            popExitTransition = { ExitTransition.None }
         ) { backStackEntry ->
             EditProductScreen(
                 navHostController,
