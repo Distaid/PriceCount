327aa5b3bd50b640bb122d9457e835228593b833
Distaid <88453698+Distaid@users.noreply.github.com>
2023-08-01 21:10:06 +0300

    Max text size

--- app/src/main/java/aid/distaid/pricecount/ui/SelectDialog.kt
+++ app/src/main/java/aid/distaid/pricecount/ui/SelectDialog.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -89,7 +90,9 @@ fun SelectDialog(
                                     Text(
                                         text = it.name,
                                         fontSize = 18.sp,
-                                        modifier = Modifier.padding(start = 4.dp)
+                                        modifier = Modifier.padding(start = 4.dp),
+                                        maxLines = 3,
+                                        overflow = TextOverflow.Ellipsis
                                     )
                                 }
                             }
